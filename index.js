301ccb848b0777064986695428990682b61dea96
Happiness Maina <mainahappiness361@gmail.com>
2024-12-16 17:25:50 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left.replace('px', ''), 10);
+  
+    if (left > 0) { 
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left.replace('px', ''), 10); // Parse current left position
+    const gameWidth = 400; 
+    const dodgerWidth = 40; 
+  
+  
+    if (left + dodgerWidth < gameWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (event.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
